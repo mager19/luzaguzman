181cb956d9b30eb8ddda2f91d2ae7ca5052a4605
Mario Germán Reyes C <mager19@gmail.com>
2017-10-30 15:52:56 -0500

    Añadido Favicon

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri() ?>/img/favicon.png" />
 
 	<?php wp_head(); ?>
 </head>
